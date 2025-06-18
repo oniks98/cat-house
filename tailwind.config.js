4d661298c484e08cb3e9186573f17fd46a2ba605
Yurii <oniks9821@gmail.com>
2025-06-18 16:24:08 +0300

    feat(first section): add grid layout and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,7 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Montserrat', 'Helvetica', 'sans-serif'],
-      },
-      fontSize: {
-        title: ['28px', { lineHeight: '36px', letterSpacing: '0.5px' }],
-        subtitle: ['16px', { lineHeight: '20px', letterSpacing: '0.5px' }],
-      },
-      screens: {
-        sm: '320px',
-        md: '768px',
-        xl: '1440px',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
